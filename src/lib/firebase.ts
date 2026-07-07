a8cbca2444cdcfac2f806e0e8b3db60aaa8faa8d
diego-zuniga21 <zunigadiego718@gmail.com>
2026-07-07 04:19:25 -0400

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,20 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE_BUCKET",
-  messagingSenderId: "TU_MESSAGING_ID",
-  appId: "TU_APP_ID",
+  apiKey: "AIzaSyDRmtqMMnn5rO6KQ57NedenLPlSjAkY6YQ",
+  authDomain: "gasfiteria-integral.firebaseapp.com",
+  projectId: "gasfiteria-integral",
+  storageBucket: "gasfiteria-integral.firebasestorage.app",
+  messagingSenderId: "413897244243",
+  appId: "1:413897244243:web:b7c4a14b5802ffadf990a6",
+  measurementId: "G-HBG03LQW3T"
 };
-
 
 const app = initializeApp(firebaseConfig);
 
-
 export const auth = getAuth(app);
-
 export const db = getFirestore(app);
